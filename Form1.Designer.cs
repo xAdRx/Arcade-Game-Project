0c97bda384269eacb9f65d7f601c17fd7a2a5478
SNRB <norbi.pujan@gmail.com>
2021-03-23 12:17:13 +0100

    Initial 01

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace CometShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Protagonist = new System.Windows.Forms.PictureBox();
+            this.Obstacle01 = new System.Windows.Forms.PictureBox();
+            this.Score = new System.Windows.Forms.Label();
+            this.Points = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Protagonist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obstacle01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Protagonist
+            // 
+            this.Protagonist.Image = global::CometShooter.Properties.Resources.Player;
+            this.Protagonist.Location = new System.Drawing.Point(61, 236);
+            this.Protagonist.Name = "Protagonist";
+            this.Protagonist.Size = new System.Drawing.Size(291, 178);
+            this.Protagonist.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Protagonist.TabIndex = 0;
+            this.Protagonist.TabStop = false;
+            // 
+            // Obstacle01
+            // 
+            this.Obstacle01.Image = global::CometShooter.Properties.Resources.Obstacle;
+            this.Obstacle01.Location = new System.Drawing.Point(626, 365);
+            this.Obstacle01.Name = "Obstacle01";
+            this.Obstacle01.Size = new System.Drawing.Size(204, 370);
+            this.Obstacle01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Obstacle01.TabIndex = 2;
+            this.Obstacle01.TabStop = false;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Location = new System.Drawing.Point(-203, -29);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(38, 15);
+            this.Score.TabIndex = 3;
+            this.Score.Text = "label1";
+            // 
+            // Points
+            // 
+            this.Points.AutoSize = true;
+            this.Points.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Points.Location = new System.Drawing.Point(61, 44);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(98, 29);
+            this.Points.TabIndex = 4;
+            this.Points.Text = "label1";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 20;
+            this.Timer.Tick += new System.EventHandler(this.JumpTime);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(1158, 728);
+            this.Controls.Add(this.Points);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Obstacle01);
+            this.Controls.Add(this.Protagonist);
+            this.Name = "Form1";
+            this.Text = "Project Comet Shooter";
+            ((System.ComponentModel.ISupportInitialize)(this.Protagonist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obstacle01)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Protagonist;
+        private System.Windows.Forms.PictureBox Obstacle01;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label Points;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
